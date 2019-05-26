cc73d3a19a03df23ee47f9bf9ea02e14a63f3a34
Victor <victorbologna@hotmail.com>
2019-05-26 13:44:34 -0300

    Implementation 1

--- app/src/main/java/com/example/helloworld/CadastrarActivity.java
+++ app/src/main/java/com/example/helloworld/CadastrarActivity.java
@@ -1,37 +1,95 @@
 package com.example.helloworld;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import static com.example.helloworld.R.id.datavis;
+
 public class CadastrarActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.helloworld.MainActivity";
+    public static final String MainActivity = "com.example.helloworld.MainActivity";
+
+    private static final String TAG = "CadastrarActivity";
+
+    private TextView mDisplayDate;
+    private ImageButton mSetDate;
+    private DatePickerDialog.OnDateSetListener mOnDateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+
+        mDisplayDate = (TextView) findViewById(R.id.datavis);
+        mSetDate = (ImageButton) findViewById(R.id.data);
+
+        mSetDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        CadastrarActivity.this,
+                        android.R.style.Theme,
+                        mOnDateSetListener,
+                        day,month,year);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+
+                String date = dayOfMonth + "/" + month + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Milena Suemi Uehara", "Felipe Wagner Caleme", "Everton", "Felipe Lugarinho", "Ninguem"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-
-        final EditText dataField = (EditText) findViewById(R.id.data);
+        Intent intent = new Intent();
+        final TextView dataField = (TextView) findViewById(R.id.datavis);
         String data = dataField.getText().toString();
-
-        intent.putExtra(EXTRA_MESSAGE, data);
-        startActivity(intent);
+        intent.putExtra(MainActivity, data);
+        setResult(Activity.RESULT_OK,intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.helloworld;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.helloworld.CadastrarActivity";
+    public static final String Cadastrar = "com.example.helloworld.CadastrarActivity";
+    private TextView dataField;
 
 
     @Override
@@ -17,24 +22,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Pega a intent de outra activity
-        Intent it = getIntent();
+        dataField = (TextView) findViewById(R.id.textView13);
 
-        //Recuperei a string da outra activity
-        String data = it.getStringExtra("data");
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
 
-        if(data != "") {
-            recoverData(data);
-        }
-    }
+        String date = day + "/" + month + "/" + year;
+        dataField.setText(date);
 
-    public void recoverData(String data) {
-        TextView dataField = (TextView) findViewById(R.id.textView13);
-        dataField.setText(data);
     }
 
     public void sendMessage(View view) {
+
         Intent intent = new Intent(this, CadastrarActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String result = data.getStringExtra("MainActivity");
+                dataField = (TextView) findViewById(R.id.textView13);
+                dataField.setText(result);
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
 }
