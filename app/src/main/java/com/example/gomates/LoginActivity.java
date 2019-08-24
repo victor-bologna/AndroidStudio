92cbc19eb3a8e85c310ff5677afccbd66beb3668
Victor <victorbologna@hotmail.com>
2019-08-24 15:48:18 -0300

    Added Login with google, phone and email; Fixed photo upload and added logout button on main activity.

--- app/src/main/java/com/example/gomates/LoginActivity.java
+++ app/src/main/java/com/example/gomates/LoginActivity.java
@@ -1,113 +1,85 @@
 package com.example.gomates;
 
 import android.content.Intent;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText email;
-    private EditText senha;
-    private TextView checkEmail;
+    private static final int MY_REQUEST_CODE = 200;
     private FirebaseAuth mAuth;
+
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
 
-        email = findViewById(R.id.email);
-        senha = findViewById(R.id.senha);
-        checkEmail = findViewById(R.id.checkEmail);
+        // Init Provider
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(), // Email
+                new AuthUI.IdpConfig.PhoneBuilder().build(), // Phone
+                new AuthUI.IdpConfig.GoogleBuilder().build() // Google
+        );
 
-        Button login = findViewById(R.id.login);
-        Button cadastrar = findViewById(R.id.cadastrar);
+        showSignInOptions();
 
-        login.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
+    }
 
-                signIn(email.getText().toString(), senha.getText().toString());
+    private void showSignInOptions() {
 
-            }
-        });
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.MyTheme)
+                .setIsSmartLockEnabled(true)
+                .build(), MY_REQUEST_CODE
+        );
 
-        cadastrar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-
-                if(createAccount(email.getText().toString(), senha.getText().toString())) {
-
-                    checkEmail.setText("Para ativar sua conta, clique no link enviado na sua conta de email!");
-
-                }
-            }
-        });
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MY_REQUEST_CODE) {
 
-        if(currentUser != null) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK) {
 
-//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//            startActivity(intent);
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
 
+            } else {
+
+                Toast.makeText(this, ""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
         }
-    }
-
-    public boolean createAccount(String email, String password) {
-        final boolean[] ret = new boolean[1];
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    ret[0] = true;
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(LoginActivity.this, task. getException().getMessage().toString(), Toast.LENGTH_SHORT).show();
-                    ret[0] = false;
-                }
-
-                // ...
-            }
-        });
-        return ret[0];
-    }
-
-    public void signIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Toast.makeText(LoginActivity.this, "Login incorretos!", Toast.LENGTH_SHORT).show();
-                }
-
-                // ...
-            }
-        });
     }
 }
--- app/src/main/java/com/example/gomates/MainActivity.java
+++ app/src/main/java/com/example/gomates/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.gomates;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,16 +40,43 @@ public class MainActivity extends AppCompatActivity {
     private static final int RESULT_LOAD_CADASTRAR = 2;
     private int activityInfo;
     private FirebaseUser user;
+    private Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        avatar = (ImageView) findViewById(R.id.avatar);
         user = FirebaseAuth.getInstance().getCurrentUser();
         usuario = (TextView) findViewById(R.id.usuario);
+        signOut = (Button) findViewById(R.id.singout);
+        final Intent intent2 = new Intent(MainActivity.this, LoginActivity.class);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(MainActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        startActivity(intent2);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT);
+                        startActivity(intent2);
+
+                    }
+                });
+            }
+        });
 
         if(user != null) {
+
+            getPicture(user);
 
             if(user.getPhotoUrl() != null)
                 avatar.setImageURI(user.getPhotoUrl());
@@ -53,6 +96,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void getPicture(FirebaseUser user) {
+
+        if(user.getPhotoUrl() != null)
+            Picasso.get().load(user.getPhotoUrl()).into(avatar);
+        else
+            Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(avatar);
+
+    }
 
 
     public void sendMessage(View view) {
@@ -78,6 +129,17 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null && activityInfo == RESULT_LOAD_IMAGE) {
             Uri selectedUri = data.getData();
             avatar.setImageURI(selectedUri);
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setPhotoUri(data.getData())
+                    .build();
+            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("Info", "User profile updated.");
+                    }
+                }
+            });
         }
 
     }//onActivityResult
